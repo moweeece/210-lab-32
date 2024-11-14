b80c893a442c34adaac0807ad48785c55686acbb
moweeece <mjespinosa990@gmail.com>
2024-11-13 17:48:18 -0800

    Update 2

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// COMSC-210 | Mauricio Espinosa | Lab 32
+// IDE Used: Visual Studio Code & Github
+
+#include "Car.h"
+#include <deque>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+// global constant declarations
+const int INITIAL_NUM_CARS = 2;
+const double CAR_PAY_PROBABILITY = 0.55;
+const double CAR_JOIN_PROBABILITY = 0.45;
+
+
+int main() {
+    srand(time(0));    // seed time for random
+
+    // Initialize a deque structure of Cars
+    deque<Car> tollBoothLane;
+
+
+
+
+
+
+    return 0;
+}
